8cadafe354c5737f0145380de25cc6d80621496a
PJTURP <821042@mystma.org>
2021-09-13 09:39:16 -0500

    I changed my python to top five movies

---
+++ main.py
@@ -0,0 +1,10 @@
+#Print 1st movie
+print("Black Sheep")
+#Print 2nd movie
+print("Water Boy")
+#Print 3rd movie
+print("Real Steel")
+#Print 4th movie 
+print("Ace Ventura")
+#Print 5th movie 
+print("Tommy Boy")
