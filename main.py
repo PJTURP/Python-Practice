3458b579803eeaec5b7dfb64e71ad46f5954d73d
PJTURP <821042@mystma.org>
2021-09-15 09:49:05 -0500

    A lot of fun changes

--- main.py
+++ main.py
@@ -17,82 +17,105 @@ def topfivemovies():
     #print fifth movie 
     print("Tommy Boy")
 
-
-
+#define the function
 def topfivesports():
 
-
+    #print sport one
     print("NASCAR")
 
-
+    #print sport two
     print("Swiming")
 
-
+    #print sport three
     print("Snowboarding")
 
-
+    #print sport four
     print("Motorcross")
 
-
+    #print sport five
     print("Drift Racing")
 
-
+#define the function
 def topfivegames():
 
-
+    #print game one
     print("GTA 5")
 
-
+    #print game two
     print("Battlefeild 4")
 
-
+    #print game three
     print("Forza Horison 4")
 
-
+    #print game four
     print("Wii Resort")
 
-
+    #print game five
     print("COD Zombies")
 
-
+#define the function
 def topfivecars():
 
-
+    #print car one
     print("2001 Mustang GT")
 
-
+    #print car two
     print("1969 Mustang Fastback")
 
-
+    #print car three
     print("GMC Cyclone")
 
-
+    #print car four
     print("Datona Challenger")
 
-
+    #print car five
     print("Pontiac Superbird")
 
-
+#define the function
 def topfiverestaraunts():
 
-
+    #print restaurant one
     print("Subway")
 
-
+    #print restaurant two
     print("Five Guys")
     
-
+    #print restaurant three
     print("Jonny Rockets")
     
-
+    #print restaurant four
     print("Culvers")
     
-    #print fifth restaurant
+    #print restaurant five
     print("Fredys")
 
+def topfiveall():
 
-#print these are my top five movies
-print("These Are My Top Five Movies")
+    topfivecars()
 
-#topcivemovies()
-topfivemovies()
+    topfivegames()
+
+    topfivemovies()
+
+    topfiverestaraunts()
+
+    topfivesports()
+
+#print these are my top five 
+print("These Are My Top Five")
+
+#topciveall()
+topfiveall()
+
+
+
+#define a function that outputs your name
+#followed by "is my name"
+def mynameis(turtle):
+
+    #output string
+    print(turtle + "is my name.")
+
+#run function
+
+mynameis("Payton")
