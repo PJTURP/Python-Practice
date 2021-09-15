fb6c391dee824588aa5ab73938c08ed61e0d18ed
PJTURP <821042@mystma.org>
2021-09-15 09:12:42 -0500

    Top five evererything

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+
+#define the function
 def topfivemovies():
     
     #print first movie
@@ -15,7 +17,81 @@ def topfivemovies():
     #print fifth movie 
     print("Tommy Boy")
 
-#print these are my top five moviees
+
+
+def topfivesports():
+
+
+    print("NASCAR")
+
+
+    print("Swiming")
+
+
+    print("Snowboarding")
+
+
+    print("Motorcross")
+
+
+    print("Drift Racing")
+
+
+def topfivegames():
+
+
+    print("GTA 5")
+
+
+    print("Battlefeild 4")
+
+
+    print("Forza Horison 4")
+
+
+    print("Wii Resort")
+
+
+    print("COD Zombies")
+
+
+def topfivecars():
+
+
+    print("2001 Mustang GT")
+
+
+    print("1969 Mustang Fastback")
+
+
+    print("GMC Cyclone")
+
+
+    print("Datona Challenger")
+
+
+    print("Pontiac Superbird")
+
+
+def topfiverestaraunts():
+
+
+    print("Subway")
+
+
+    print("Five Guys")
+    
+
+    print("Jonny Rockets")
+    
+
+    print("Culvers")
+    
+    #print fifth restaurant
+    print("Fredys")
+
+
+#print these are my top five movies
 print("These Are My Top Five Movies")
 
 #topcivemovies()
