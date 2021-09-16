950d526905906f2e9a2a371ee5a7458365528826
PJTURP <821042@mystma.org>
2021-09-16 09:06:20 -0500

    oading.....

--- main.py
+++ main.py
@@ -1,108 +1,11 @@
+#define a function that outputs your name
+#followed by "is my name"
 
-#define the function
-def topfivemovies():
-    
-    #print first movie
-    print("Black Sheep")
-    
-    #print second movie
-    print("Water Boy")
-    
-    #print theird movie
-    print("Real Steel")
-    
-    #print fourth movie 
-    print("Ace Ventura")
-    
-    #print fifth movie 
-    print("Tommy Boy")
+def myNameIs(turtle):
 
-#define the function
-def topfivesports():
+    #output string
+    print(turtle + " is my name.")
 
-    #print sport one
-    print("NASCAR")
+#run function
 
-    #print sport two
-    print("Swiming")
-
-    #print sport three
-    print("Snowboarding")
-
-    #print sport four
-    print("Motorcross")
-
-    #print sport five
-    print("Drift Racing")
-
-#define the function
-def topfivegames():
-
-    #print game one
-    print("GTA 5")
-
-    #print game two
-    print("Battlefeild 4")
-
-    #print game three
-    print("Forza Horison 4")
-
-    #print game four
-    print("Wii Resort")
-
-    #print game five
-    print("COD Zombies")
-
-#define the function
-def topfivecars():
-
-    #print car one
-    print("2001 Mustang GT")
-
-    #print car two
-    print("1969 Mustang Fastback")
-
-    #print car three
-    print("GMC Cyclone")
-
-    #print car four
-    print("Datona Challenger")
-
-    #print car five
-    print("Pontiac Superbird")
-
-#define the function
-def topfiverestaraunts():
-
-    #print restaurant one
-    print("Subway")
-
-    #print restaurant two
-    print("Five Guys")
-    
-    #print restaurant three
-    print("Jonny Rockets")
-    
-    #print restaurant four
-    print("Culvers")
-    
-    #print restaurant five
-    print("Fredys")
-
-def topfiveall():
-
-    topfivecars()
-
-    topfivegames()
-
-    topfivemovies()
-
-    topfiverestaraunts()
-
-    topfivesports()
-
-#print these are my top five 
-print("These Are My Top Five")
-
-#topciveall()
-topfiveall()
+myNameIs("Payton")
