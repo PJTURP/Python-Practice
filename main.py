c4dc1f220ad6b72c4bddae2b4aa6faafae90ad88
PJTURP <821042@mystma.org>
2021-09-17 08:53:18 -0500

    Mcqueen

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 
     #Madlib Function
-def madlib(a,b,c,d,e,f,g,h,i,j,k):
-    print("Star Wars is a" + a + b + "of" + c + "far far away. There are "+ d +"battles between" + e + f + "in" + g +"space and" + h + "duels with" + i + "called" + j + "sabers.") 
+def madlib(a,b,c,d,e,f,g,h,i,j):
+    print("Star Wars is a" + a + b + " of" + c + " far far away. There are "+ d +"battles between" + e + f + "in" + g +"space and" + h + "duels with" + i + "called" + j + "sabers.") 
         
-madlib("School, scary, wierd, plain, cars, lazy, silly, i, blank, k) 
+madlib(" planet", " full", " cans", "confused ", " lightning", " mcqueen ", " blank ", " poolnoodle ", " batman ", " plain ")
