193cef52ac159a7772ef44ba1d05cb6bb2a3a591
PJTURP <821042@mystma.org>
2021-09-17 09:06:11 -0500

    Fixed the a problem

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 
     #Madlib Function
-def madlib(a,b,c,d,e,f,g,h,i,j):
-    print("Star Wars is a" + a + b + " of" + c + " far far away. There are "+ d +"battles between" + e + f + "in" + g +"space and" + h + "duels with" + i + "called" + j + "sabers.") 
+def madlib(a,b,c,d,e,f,g,h,i,j, k):
+    print("Star Wars is a" + a + b + " of " + c +  " versus evil in a" + d + " far far away. There are "+ e +"battles between" + f + g + "in" + h +"space and" + i + "duels with" + j + "called" + k + "sabers.") 
         
-madlib(" planet", " full", " cans", "confused ", " lightning", " mcqueen ", " blank ", " poolnoodle ", " batman ", " plain ")
+madlib(" planet", " full", "good", " year" ,"confused ", " lightning", " mcqueen ", " blank ", " poolnoodle ", " batman ", " plain ")
