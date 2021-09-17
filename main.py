c1a90f7fa67844cd363014ec93d3ff343e7467a0
PJTURP <821042@mystma.org>
2021-09-17 09:13:39 -0500

    goodnight

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 
     #Madlib Function
 def madlib(a,b,c,d,e,f,g,h,i,j, k):
-    print("Star Wars is a" + a + b + " of " + c +  " versus evil in a" + d + " far far away. There are "+ e +"battles between" + f + g + "in" + h +"space and" + i + "duels with" + j + "called" + k + "sabers.") 
+    print("Star Wars is a" + a + b + " of " + c +  " versus evil in a" + d + " far far away."
+     "There are "+ e +"battles between" + f + g + "in" + h +"space and" + i + "duels with" + j + "called" + k + "sabers.") 
         
-madlib(" planet", " full", "good", " year" ,"confused ", " lightning", " mcqueen ", " blank ", " poolnoodle ", " batman ", " plain ")
+madlib(" planet", " full", "good", " year" ,"confused ", " lightning", " mcqueen ", " blank ",
+ " poolnoodle ", " batman ", " plain ")
