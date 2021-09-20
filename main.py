40f23ec5081c426008e666ea2be481ee13d4b43e
PJTURP <821042@mystma.org>
2021-09-20 09:13:53 -0500

    lolol

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 
-    #Madlib Function
-def madlib(a,b,c,d,e,f,g,h,i,j, k):
-    print("Star Wars is a" + a + b + " of " + c +  " versus evil in a" + d + " far far away."
-     "There are "+ e +"battles between" + f + g + "in" + h +"space and" + i + "duels with" + j + "called" + k + "sabers.") 
-        
-madlib(" planet", " full", "good", " year" ,"confused ", " lightning", " mcqueen ", " blank ",
- " poolnoodle ", " batman ", " plain ")
+#def function woof
+def woof():
+    return "Woof"
+
+print (woof()) 
+
+#def cat and dog
+def cat():
+    return "cat"
+
+def dog():
+    return "dog"
+
+#print cat + dog 
+print(cat() + dog())
+
+def numberOfPlanets():
+    return 8
+
+print(numberOfPlanets())
