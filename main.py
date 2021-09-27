53c4c81cc360fd5d517c3ec3cedb67bc70d2a4c6
PJTURP <821042@mystma.org>
2021-09-27 09:47:57 -0500

    Learning

--- main.py
+++ main.py
@@ -1,15 +1,42 @@
+#Homemade dictionary
+def define(word):
 
-def equalsTen(x):
-    if x==10:
-        
-        return "x equals 10"
-    
-    elif x > 10:
-        return "x is greater than 10"
+    #word lowercase
+    word = word.lower()
 
+#1
+    if word=="school":
+        return "A place of learning"
+#2
+    elif word=="starwars":
+        return "A place far far away a long time ago"
+#3
+    elif word=="racoon":
+        return "Trash panda"
+#4  
+    elif word=="v8":
+        return "An 8 cylinder driven engine in a V configuration"
+#5
+    elif word=="minecraft":
+        return "A perfect chieldhood"
+#6
+    elif word=="Pencil":
+        return "A wood writing untencil"
+#7
+    elif word=="Homework":
+        return "A teachers good idea"
+#8
+    elif word=="Landmine":
+        return "The fordiden Tuna"
+#9
+    elif word=="Dictionary":
+        return "A book with no pictures"
+#10
+    elif word=="Car Parts":
+        return "The ability to buy happiness"
+#11
     else:
-       
-        return "x is not equal to 10"
-       
- 
-print(equalsTen(10))
+        return "Your word dose not exist, leave."
+
+#run dictionary
+print(define("lol"))
